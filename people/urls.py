b542b5609124af338eca90d7ec6e17b75fcdac7a
LEAKONO <leakonoemmanuel3@gmail.com>
2025-06-27 00:02:48 +0300

    Add API views for listing and creating Person instances

--- people/urls.py
+++ people/urls.py
@@ -1,5 +1,8 @@
+
 from django.urls import path
 from . import views
-urlpatterns=[
-    path('',views.Home ,name='home')
+
+urlpatterns = [
+    path('people/', views.get_all_people, name='get_all_people'),
+    path('people/create/', views.create_person, name='create_person'),
 ]
--- people/views.py
+++ people/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Person
+from.serializers import PersonSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
-def Home(request):
-    return HttpResponse ("Hello world")
+@api_view(['GET'])
+def get_all_people(request):
+    people=Person.objects.all()
+    serializer=PersonSerializer(people,many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def create_person(request):
+    serializer=PersonSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
+
+
